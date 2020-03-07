25fe1c72c8b488002dc26cd202065c9e5663f5c5
franky3020 <210238hwsh@gmail.com>
2020-03-07 14:13:03 +0800

    解釋單元測試過程

--- app/src/test/java/ChangeChar/HtoaTest.java
+++ app/src/test/java/ChangeChar/HtoaTest.java
@@ -9,7 +9,7 @@ public class HtoaTest {
     @Test
     public void doChange() {
         Htoa htoa = new Htoa();
-        assertEquals("a", htoa.doChange("H"));
-        assertEquals("g", htoa.doChange("g"));
+        assertEquals("a", htoa.doChange("H"));//測試輸入字串參數 H 時，會不會輸出a
+        assertEquals("g", htoa.doChange("g"));//測試輸入字串參數 g 時，會不會輸出g
     }
 }
