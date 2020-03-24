816f79ac112d0afce19a2cf46cb716c7122b84c0
benny <yuyuabc1999@gmail.com>
2020-03-24 20:33:02 +0800

    新增TaskTime物件

--- app/src/main/java/Task/Task.java
+++ app/src/main/java/Task/Task.java
@@ -4,13 +4,13 @@ import java.sql.Array;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Task {
+public class Task extends TaskTime{
     private String taskName;
     private String taskID;
     private String taskAssigner;
     private String taskAccepter;
     private int taskState;/* 訂單狀態 0=發布中 1=已接受 2=已完成 */
-    private String year,month,day;/* 訂單日期 包含年月日 待優化應該可以有更好的寫法 */
+    private TaskTime taskTime = new TaskTime();/* 訂單日期 包含年月日 */
 
     public Task(String taskName,String taskID,String taskAssigner,String taskAccepter){
         /* 基本Constructor */
@@ -18,11 +18,10 @@ public class Task {
         this.taskID = taskID;
         this.taskAssigner = taskAssigner;
         this.taskAccepter = taskAccepter;
-        taskState = 0;
-        year = "2020";
-        month = "03";
-        day = "24";
-
+        this.taskState = 0;
+        this.setYear("2020");
+        this.setMonth("03");
+        this.setDay("24");
     }
 
     public void setTaskName(String taskName) {
@@ -41,18 +40,6 @@ public class Task {
     }
     public void setTaskState(int taskState){
         this.taskState = taskState;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
-
-    public void setMonth(String month) {
-        this.month = month;
-    }
-
-    public void setDay(String day) {
-        this.day = day;
     }
 
     public String getTaskName() {
@@ -75,15 +62,4 @@ public class Task {
         return taskState;
     }
 
-    public String getYear() {
-        return year;
-    }
-
-    public String getMonth() {
-        return month;
-    }
-
-    public String getDay() {
-        return day;
-    }
 }
