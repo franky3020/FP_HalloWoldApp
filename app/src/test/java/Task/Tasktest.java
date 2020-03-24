db44d5b8ed866aeee7850f887ba8062f716e8b57
benny <yuyuabc1999@gmail.com>
2020-03-24 17:17:02 +0800

    Task物件新增屬性

--- app/src/test/java/Task/Tasktest.java
+++ app/src/test/java/Task/Tasktest.java
@@ -7,14 +7,40 @@ import static org.junit.Assert.*;
 
 public class Tasktest {
     @Test
+    public void doConstruct(){
+        Task task = new Task("Buying Lunch","A001","May","John");
+
+        assertEquals("Buying Lunch",task.getTaskName());
+        assertEquals("A001",task.getTaskID());
+        assertEquals("May",task.getTaskAssigner());
+        assertEquals("John",task.getTaskAccepter());
+        assertEquals(0,task.getTaskState());
+        assertEquals("2020",task.getYear());
+        assertEquals("03",task.getMonth());
+        assertEquals("24",task.getDay());
+
+    }
+    @Test
     public void doSet(){
-        Task task = new Task();
+        Task task = new Task("Buying Lunch","A001","May","John");
 
         task.setTaskName("Cleaning room");
-        task.setTaskID("A001");
+        task.setTaskID("A002");
+        task.setTaskAssigner("Jack");
+        task.setTaskAccepter("Mary");
+        task.setTaskState(1);
+        task.setYear("2020");
+        task.setMonth("03");
+        task.setDay("22");
 
         assertEquals("Cleaning room",task.getTaskName());
-        assertEquals("A001",task.getTaskID());
+        assertEquals("A002",task.getTaskID());
+        assertEquals("Jack",task.getTaskAssigner());
+        assertEquals("Mary",task.getTaskAccepter());
+        assertEquals(1,task.getTaskState());
+        assertEquals("2020",task.getYear());
+        assertEquals("03",task.getMonth());
+        assertEquals("22",task.getDay());
 
     }
 }
