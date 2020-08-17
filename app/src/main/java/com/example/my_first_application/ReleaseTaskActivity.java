eac5a6514794fd59d7bf0315fa008945942d8f99
franky3020 <210238hwsh@gmail.com>
2020-08-18 00:13:56 +0800

    加app bar 圖示 跟 +任務按鈕

--- app/src/main/java/com/example/my_first_application/ReleaseTaskActivity.java
+++ app/src/main/java/com/example/my_first_application/ReleaseTaskActivity.java
@@ -1,5 +1,6 @@
 package com.example.my_first_application;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,6 +19,9 @@ public class ReleaseTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_release_task);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
     }
