0228f5b813a96d226f2d0240d7591b7803ae8682
benny <yuyuabc1999@gmail.com>
2020-08-23 23:43:48 +0800

    完成發布任務(postTime可再改善)

--- app/src/main/java/Task/TaskApiService.java
+++ app/src/main/java/Task/TaskApiService.java
@@ -13,7 +13,8 @@ import okhttp3.Response;
 public class TaskApiService {
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
-    public void post(final String taskName) { //未完成
+    public void post(final String taskName, final String message, final String salary , final String dateTime
+                        , final String taskType, final String taskAddress, final String taskCity) {
         final String TAG = "franky-test";
         new Thread(new Runnable() {
             @Override
@@ -24,13 +25,22 @@ public class TaskApiService {
 
                     RequestBody body = RequestBody.create("", JSON);
 
-                    String base_URL = "http://140.134.26.71:46557/ms-provider-develop/tasks?";
-                    String taskParameter1 = "taskName=" + taskName;
+                    //String base_URL = "http://140.134.26.71:46557/ms-provider-develop/tasks?";
+                    String base_URL = "http://140.134.26.71:46557/ms-provider-test-benny-branch-api/tasks?";
+                    String taskParameter1 = "TaskName=" + taskName;
+                    String taskParameter2 = "Message=" + message;
+                    String taskParameter3 = "PostTime=" + dateTime + " 00:00:00";
+                    String taskParameter4 = "Salary=" + salary;
+                    String taskParameter5 = "TypeName=" + taskType;
+                    String taskParameter6 = "TaskAddress=" + taskAddress;
+                    String taskParameter7 = "TaskCity=" + taskCity;
 
 
 
                     Request request = new Request.Builder()
-                            .url(base_URL + taskParameter1 + "&message=FromAndroidTest&postTime=2020-07-14 00:00:00&salary=777")
+                            .url(base_URL + taskParameter1 + "&" + taskParameter2 + "&" + taskParameter3 +
+                                    "&" + taskParameter4 + "&" + taskParameter5 + "&" + taskParameter6 +
+                                    "&" + taskParameter7)
                             .post(body)
                             .build();
                     Response response = client.newCall(request).execute();
