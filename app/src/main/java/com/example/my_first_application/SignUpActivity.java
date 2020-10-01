09128a995f6274aea8c3f8f11c2651a7e325cf4c
yvaineChen <ivywaz@gmail.com>
2020-10-01 23:31:19 +0800

    新增 Toolbar

--- app/src/main/java/com/example/my_first_application/SignUpActivity.java
+++ app/src/main/java/com/example/my_first_application/SignUpActivity.java
@@ -1,24 +1,32 @@
 package com.example.my_first_application;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = LoginActivity.class.getSimpleName();
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(LOG_TAG, "onStart");
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_sign_up);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 
 }
