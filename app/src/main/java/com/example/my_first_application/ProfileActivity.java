efa15f5dc8e98dd71ec4a05d845b8791f78782c4
yvaineChen <ivywaz@gmail.com>
2020-10-13 07:09:48 +0800

    實作 Bottom Navigation Bar 功能

--- app/src/main/java/com/example/my_first_application/ProfileActivity.java
+++ app/src/main/java/com/example/my_first_application/ProfileActivity.java
@@ -1,8 +1,14 @@
 package com.example.my_first_application;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -13,5 +19,25 @@ public class ProfileActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_profile);
         setSupportActionBar(toolbar);
+
+        BottomNavigationView bottomNavigationView
+                = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.icon_home:
+                        Intent intent = new Intent();
+                        intent.setClass(ProfileActivity.this, HomePageActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.icon_search:
+                        break;
+                    case R.id.icon_message:
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
