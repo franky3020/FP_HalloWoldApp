fe9799fbce8853d7dd4631d65ec7d5cb7d7cc44c
franky3020 <210238hwsh@gmail.com>
2020-10-15 00:26:15 +0800

    加部分登入功能

--- app/src/main/java/com/example/my_first_application/LoginActivity.java
+++ app/src/main/java/com/example/my_first_application/LoginActivity.java
@@ -2,14 +2,24 @@ package com.example.my_first_application;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = LoginActivity.class.getSimpleName();
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onStart() {
@@ -26,6 +36,33 @@ public class LoginActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public void onClickToSingIn(View view) {
+        String email = "m2315zx1@yahoo.com.tw";
+        String password = "m2315zx1@yahoo.com.tw"; // 因為目前註冊帳密都是email
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(LOG_TAG, "signInWithEmail:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(LOG_TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            // ...
+                        }
+
+                        // ...
+                    }
+                });
     }
 
     @Override
