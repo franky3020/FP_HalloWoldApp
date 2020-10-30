96eba869b5a28c0b12358f7cf44acae5e6b4ceb5
franky3020 <210238hwsh@gmail.com>
2020-10-30 21:57:39 +0800

    加狀態註解, 之後須修正狀態為五種

--- app/src/main/java/Task/TaskStateEnum.java
+++ app/src/main/java/Task/TaskStateEnum.java
@@ -5,11 +5,13 @@ import java.util.Map;
 
 public enum TaskStateEnum { // 此內容 跟 後端的類別完全一致 Todo 還需要要加上缺少的狀態 Worker要
 
-    BOOS_RELEASE_AND_SELECTING_WORKER(1),
-    BOOS_SELECTED_WORKER(2),
-    WORKER_CANCEL_REQUEST(3),  // 這要想一下
+    // 只需要五種狀態
+
+    BOOS_RELEASE_AND_SELECTING_WORKER(1), // 需要
+    BOOS_SELECTED_WORKER(2),  // 需要
+    WORKER_CANCEL_REQUEST(3),
     BOOS_CANCEL_RELEASE(4),
-    WORKER_CONFIRM_EXECUTION(5),
+    WORKER_CONFIRM_EXECUTION(5), // 需要   要改名成 Task Doing
     WORKER_REQUEST_CHECK_THE_TASK_IS_DONE(6),
     BOOS_ACCOUNT_THE_TASK_NOT_DONE(7),
     BOOS_ACCOUNT_THE_TASK_IS_DONE(8),
@@ -20,6 +22,11 @@ public enum TaskStateEnum { // 此內容 跟 後端的類別完全一致 Todo �
     WORKER_STOP_TASK(13),
     END(14),
     BOOS_CANCEL_SELECTED_THE_WORKER(15);
+
+    // 需要 等待Worker同意中止任務
+    // 需要 等待Boos確認完成此任務
+
+
 
 
     private int value;
