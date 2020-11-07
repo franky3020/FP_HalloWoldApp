0d62be82928938e518dc0201aefbbf38a96fda55
franky3020 <210238hwsh@gmail.com>
2020-11-07 19:55:08 +0800

    clean code

--- app/src/main/java/com/example/my_first_application/CollectTaskActivity.java
+++ app/src/main/java/com/example/my_first_application/CollectTaskActivity.java
@@ -5,12 +5,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import android.os.Bundle;
 
